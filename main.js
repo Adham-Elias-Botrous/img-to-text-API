097365c75e8353bee0aba65688e2e6b21cb44c5e
Adham Elias Botrous <50738429+Adham-Elias-Botours@users.noreply.github.com>
2020-05-28 17:18:00 +0200

    style and few lay out adjusment

--- main.js
+++ main.js
@@ -4,28 +4,31 @@ const showData = document.getElementById('showData');
 
 myForm.addEventListener('submit', (ev) => {
   ev.preventDefault();
-  // const endPoint = 'https://api.ocr.space/parse/image';
+  if (inpFile.value.trim() === '') {
+    return (showData.innerHTML =
+      '<div class="alert">You did not choose any file! <br> Chose one then click on the button.</div>');
+  }
   const endPoint =
     'https://custom-ocr.klippa.com/api/v1/parseDocument?X-Auth-Key=sLiopG4bfDH6wcIZxBjZk85Z9DIiUK7G';
 
   const formData = new FormData();
-  // formData.append('inpFile', inpFile.files[0]);
   formData.append('document', inpFile.files[0]);
-  // formData.append('language', 'dut');
-  // formData.append('isTable', 'true');
   const myHeaders = new Headers();
-  // myHeaders.append('apikey', 'd0bef30f5388957');
+
   fetch(endPoint, { method: 'post', body: formData, headers: myHeaders })
-    .then((response) => response.json())
-    .then((result) => {
-      console.log(result);
-      // showData.innerHTML = '';
-      // result.ParsedResults[0].TextOverlay.Lines.forEach((line) => {
-      //   const lineBox = document.createElement('div');
-      //   lineBox.classList.add('line-box');
-      //   lineBox.innerText = line.LineText;
-      //   showData.appendChild(lineBox);
-      // });
+    .then((response) => {
+      if (!response.ok)
+        throw Error(`Request rejected with status ${response.status}`);
+      return response.json();
     })
-    .catch((error) => console.log('error', error));
+    .then((result) => {
+      showData.innerHTML = '';
+      const lineBox = document.createElement('div');
+      lineBox.classList.add('line-box');
+      lineBox.innerText = result.data.raw_text;
+      showData.appendChild(lineBox);
+    })
+    .catch((error) => {
+      showData.innerHTML = `<div class="alert">${error}</div>`;
+    });
 });
