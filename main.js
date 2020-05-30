208bc46252b4508a4a1185d0624294a3922b37c3
Adham Elias Botrous <50738429+Adham-Elias-Botours@users.noreply.github.com>
2020-05-31 00:17:36 +0200

    add progress bar

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const myForm = document.getElementById('myForm');
 const inpFile = document.getElementById('inpFile');
 const showData = document.getElementById('showData');
-console.log(axios);
-let loader;
+
+const progressFunc = (progressValue) => {
+  const progressElm = `<div class="progressContainer">
+<div class="progressBar" style="width:${progressValue}%">Uploading ${progressValue}%</div>
+</div>`;
+  return progressElm;
+};
 
 myForm.addEventListener('submit', (ev) => {
   ev.preventDefault();
@@ -10,7 +15,9 @@ myForm.addEventListener('submit', (ev) => {
     return (showData.innerHTML =
       '<div class="alert">You did not choose any file! <br> Chose one then click on the button.</div>');
   }
-  showData.innerHTML = '<div class="loader"></div>';
+  let progressValue = 0;
+  showData.innerHTML = progressFunc(progressValue);
+
   const endPoint =
     'https://custom-ocr.klippa.com/api/v1/parseDocument?X-Auth-Key=sLiopG4bfDH6wcIZxBjZk85Z9DIiUK7G';
 
@@ -27,19 +34,23 @@ myForm.addEventListener('submit', (ev) => {
           return status >= 200 && status < 300;
         },
         onUploadProgress: function (progressEvent) {
-          console.log(
-            `Upload Progress: ${Math.round(
-              (progressEvent.loaded / progressEvent.total) * 100
-            )}%`
+          progressValue = Math.round(
+            (progressEvent.loaded / progressEvent.total) * 100
           );
+          if (progressValue === 100) {
+            showData.innerHTML =
+              '<p class="loaderTitle">Getting the Data</p><div class="loader"></div>';
+            return;
+          }
+          showData.innerHTML = progressFunc(progressValue);
         },
       }
     )
     .then((result) => {
-      showData.innerHTML = '';
       const lineBox = document.createElement('div');
       lineBox.classList.add('line-box');
       lineBox.innerText = result.data.data.raw_text;
+      showData.innerHTML = '';
       showData.appendChild(lineBox);
       inpFile.value = '';
     })
